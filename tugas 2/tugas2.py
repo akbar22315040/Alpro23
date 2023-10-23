3e614fe7acbfb1b39c146e5d705502425e2172bc
akbar <akbar22315040@umkendari.ac.id>
2023-10-24 05:25:52 +0700

    first init github

---
+++ tugas 2/tugas2.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+	# tupple
+	x = ( 1, "belimbing" , "apel" , "ceri")
+	# list
+	y = [ 1, "belimbing" , "apel" , "ceri"]
+	# dictionery
+	z = {1: "apel" , "b": "belimbing" , "c": "ceri"
+	}
+	print(type(x))
+	print(x)
+	print(type(y))
+	print(y)
+	print(type(z))
+	print(z)
